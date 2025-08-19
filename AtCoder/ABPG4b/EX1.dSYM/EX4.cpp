f89b86436ebaa3a8b5d7e9d5711b3fd3a0ad3d7b
Cheuwb <x3mgamer1@gmail.com>
2025-08-19 18:41:49 -0400

    beginner exmaples ex 1 - ex5

---
+++ AtCoder/ABPG4b/EX1.dSYM/EX4.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // seconds in a year
+    int seconds = 365 * 24 * 60 * 60;
+
+    // seconds in one year
+    cout << seconds << endl;
+    // how many seconds in two eyars
+    cout << seconds * 2 << endl;
+    // 5 years
+    cout << seconds * 5 << endl;
+    // seconds in 10 years
+    cout << seconds * 10 << endl;
+    return 0;
+}
--- AtCoder/practice1/p1.cpp
+++ AtCoder/practice1/p1.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    string s;
+
+    cin >> a;
+    cin >> b >> c;
+    cin >> s;
+
+    cout << (a + b + c) << " " << s << endl;
+
+    return 0;
+}
