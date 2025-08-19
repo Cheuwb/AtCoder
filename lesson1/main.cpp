0da4f4e348be3171d8bb424519fbfb893bb38844
Cheuwb <x3mgamer1@gmail.com>
2025-08-19 14:51:40 -0400

    cpp repo

---
+++ lesson1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Start program for Addition \n";
+    std::cout << "Enter number for x ";
+    int x{};
+    std::cin >> x;
+    std::cout << "Double that number is " << x * 2 << "\n";
+    return 0;
+}
